6e0ec9bfa7004f05e330b4bf943384edfa6a051c
Chris Campbell <chris@launchbadge.com>
2021-04-08 19:09:39 -0700

    wip: select

--- src/components/Base/Select.tsx
+++ src/components/Base/Select.tsx
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 
 interface Props {
-  options: string[]
+  options: string[];
+  onChange?: Function;
 }
 
-const Select: React.FC<Props> = ({ options }: Props) => {
-  const [items] = useState([...options])
-  return (<div>{options}</div>);
+const Select: React.FC<Props> = ({ options, onChange }: Props) => {
+  const [items] = useState([...options]);
+  const [value, setValue] = useState(items[0]);
+
+  function handleSelect(e: ChangeEvent<HTMLSelectElement>): void {
+    e.preventDefault();
+    setValue(e.currentTarget.value);
+
+    if (onChange != null) {
+      onChange(e.currentTarget.value);
+    }
+  }
+
+  return (
+    <select
+      className="flex flex-col items-center justify-center w-full"
+      value={value}
+      onChange={handleSelect}
+    >
+      {items.map(item => (
+        <option key={item} value={item}>{item}</option>
+      ))}
+    </select>
+  );
 }
 
 export default Select;
